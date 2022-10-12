c85305e62cd2079ff985c544c3d393498e6e8d13
A7madev <a7madev@users.noreply.github.com>
2022-10-12 13:01:01 +0300

    Update route53.sh

--- route53.sh
+++ route53.sh
@@ -3,3 +3,30 @@
 # get hosted zone
 echo "Enter hosted zone: "
 read hosted_zone
+
+# get record
+echo "Enter record: "
+read resource_record
+
+# get output
+echo "Output: (default: json)"
+read output
+output="${output:=json}"
+
+# validate hosted zone
+if [ "$hosted_zone" != *. ]
+then
+  hosted_zone="$hosted_zone."
+fi
+
+# validate resource record
+if [ "$resource_record" != *. ]
+then
+  resource_record="$resource_record."
+fi
+
+# find the zone id
+zone_id=$(aws route53 list-hosted-zones --query "HostedZones[?Name == '$hosted_zone']" | jq -c '.[] | .Id | sub("/hostedzone/"; "")' | tr -d '"')
+
+# find the record
+aws route53 list-resource-record-sets --hosted-zone-id $zone_id --query "ResourceRecordSets[?Name == '$resource_record']" --output $output
